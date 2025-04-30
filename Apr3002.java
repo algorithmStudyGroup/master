e3dae9be634b7b85bcb2863e4a44c8971b91f1c9
EY <key2938@gamil.com>
2025-04-30 18:57:05 +0900

    0430

--- Apr3002.java
+++ Apr3002.java
@@ -2,29 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main { //2193
+public class Apr3002 { //2193
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] quan = new int[n];
-        for (int i = 0; i < n; i++) {
-            quan[i] = Integer.parseInt(br.readLine());
-        }
         long[][] dp = new long[n + 2][2];
-        dp[1][0] = 1;
+        dp[1][0] = 0;
         dp[1][1] = 1;
-        dp[2][0] = 2;
-        dp[2][1] = 2;
+        dp[2][0] = 1;
+        dp[2][1] = 0;
         if (n > 2) {
             for (int i = 3; i < n + 2; i++) {
                 dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-                dp[i][1] = dp[i - 1][0] + dp[i - 2][0];
+                dp[i][1] = dp[i - 1][0];
             }
         }
-        long max = 0;
-
-
-
         long count = dp[n][0] + dp[n][1];
         System.out.println(n + ": " + count);
 
