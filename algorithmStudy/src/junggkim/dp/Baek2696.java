5bb055cc42e1fa065f13821373f555cfeffc4885
junggkim <kjk1526@naver.com>
2025-04-30 19:55:51 +0900

    Baek2696

--- algorithmStudy/src/junggkim/dp/Baek2696.java
+++ algorithmStudy/src/junggkim/dp/Baek2696.java
@@ -10,24 +10,54 @@ public class Baek2696 {
         StringTokenizer st ;
 
         int N = Integer.parseInt(br.readLine());
+        List<Integer> temp;
 
+        PriorityQueue<Integer> pq;
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
         for (int i = 0; i < N; i++){
             int M = Integer.parseInt(br.readLine());
+            temp = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < N + 1 ; j++){
-                if (j % 11 == 0){
+            System.out.println(M/2 + 1);       //소수 개수
+            int printCount = 0; // ★ 추가
+
+            pq = new PriorityQueue<>();
+
+            for (int j = 1; j < M + 1 ; j++){
+
+                if ((j - 1) % 10 == 0 && j != 1) {
+//                    System.out.println("j = " + j);
                     st = new StringTokenizer(br.readLine());
+
                 }
+
                 int num = Integer.parseInt(st.nextToken());
-                if (j %2 !=0){  //홀수 일경우
+                pq.offer(num);
+
+                if (j % 2 !=0){  //홀수 일경우
                     int index = j / 2;
-                    pq.poll();
+                    for (int k = 0; k < M; k++){
+                        int tempNum = pq.poll();
+                        if (k == index){
+                            System.out.print(tempNum + " ");
+                            printCount++;
+                            if (printCount % 10 == 0) {     // ★ 10개마다 줄바꿈
+                                System.out.println();
+                            }
+                            temp.add(tempNum);
+                            break;
+                        }
+                        temp.add(tempNum);
+                    }
+                    int l = 0;
+                    while(!temp.isEmpty()){
+                        pq.offer(temp.remove(l));
+                    }
+
                 }
 
             }
+            System.out.println();
         }
 
 
